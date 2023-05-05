394b4c4c91fa6bab138afb8015c9839a63299143
user <user@v2.raketacn.local>
2023-05-05 18:52:43 +0900

    template

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,36 +1,42 @@
- 
 import SettingsSidebar from "./settingsSidebar/SettingsSidebar";
 import SidebarComponent from "./sidebar/SidebarComponent";
 import HeaderComponent from "./header/HeaderComponent";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import "devextreme-react/text-area"; 
+import "devextreme-react/text-area";
 function Layout() {
   const [showMenu, setShowMenu] = useState(false);
   const [showSettingsMenu, setShowSettingsMenu] = useState(false);
   const [showHeader, setShowHeader] = useState(false);
+
   return (
-    <>
-      <SettingsSidebar
-        showSettingsMenu={showSettingsMenu}
-        setShowSettingsMenu={setShowSettingsMenu}
-        setShowHeader={setShowHeader}
-      />
-      <div className="flex width100">
-        <SidebarComponent showMenu={showMenu} setShowMenu={setShowMenu} />
-        <div className="contentWrapper mXauto px15 width100">
-          <HeaderComponent
-            showHeader={showHeader}
-            showMenu={showMenu}
-            setShowMenu={setShowMenu}
+    <> 
+      {location.pathname !== "/login" ? (
+        <div className="">
+          <SettingsSidebar
             showSettingsMenu={showSettingsMenu}
             setShowSettingsMenu={setShowSettingsMenu}
+            setShowHeader={setShowHeader}
           />
-          <Outlet /> 
+          <div className="flex width100">
+            <SidebarComponent showMenu={showMenu} setShowMenu={setShowMenu} />
+            <div className="contentWrapper mXauto px15 width100">
+              <HeaderComponent
+                showHeader={showHeader}
+                showMenu={showMenu}
+                setShowMenu={setShowMenu}
+                showSettingsMenu={showSettingsMenu}
+                setShowSettingsMenu={setShowSettingsMenu}
+              />
+              <Outlet />
+            </div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <Outlet />
+      )}
     </>
-  )
+  );
 }
 
 export default Layout;
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,6 +1,6 @@
 import SectionComponent from "../../components/ui/section/SectionComponent";
 import { UiButton } from "../../components/ui/button/UiButton";
-import { Checkbox } from "../../components/ui/checkbox/Checkbox"; 
+import { Checkbox } from "../../components/ui/checkbox/Checkbox";
 import { BigTitle } from "../../components/ui/title/BigTitle";
 import styles from "./Login.module.css";
 function LoginPage() {
@@ -16,55 +16,54 @@ function LoginPage() {
         "alignCenter",
       ].join(" ")}
     >
-<SectionComponent titleText="Войти">
-<form
-        action=""
-        className={[
-          "br075rem",
-          "p25",
-          "pt75",
-          "title",
-          "bgWhite",
-          'width100',
-          "flex",
-          "column",
-          "loginformWrapper" 
-        ].join(" ")}
-      >  
-        <input
-          type="text"
-          className="borderGrey my10 p15 br075rem"
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          name=""
-          className="p15 my10 br075rem borderGrey"
-          placeholder="Пароль"
-          id=""
-        />
-           <Checkbox>Запомнить</Checkbox>
-         
-        
-        <UiButton>Войти</UiButton>
-        <span className="my5">
-          Забыли пароль ?{" "}
-          <a className="colorBlue" href="">
-            Восстоновить
-          </a>
-        </span>
-        <span className="my5">
-          Не имеете аккаунта ?{" "}
-          <a className="colorBlue" href="">
-            Регистрация
-          </a>
-        </span> 
-      </form>
-</SectionComponent>
-
-</div>
-  
-  
+      <SectionComponent titleText="Войти">
+        <form
+          action=""
+          className={[
+            "br075rem",
+            "p25",
+            "pt75",
+            "title",
+            "bgWhite",
+            "width100",
+            "flex",
+            "column",
+            "loginformWrapper",
+          ].join(" ")}
+        >
+          <input
+            type="text"
+            className="borderGrey my10 p15 br075rem"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            name=""
+            className="p15 my10 br075rem borderGrey"
+            placeholder="Пароль"
+            id=""
+          />
+          {/* <Checkbox>321 </Checkbox>*/}
+          <div className="flex">
+            <input type="checkbox" name="remember" id="remember" className="scale1_5"/>
+            <label htmlFor="remember" className="px15">Запомнить</label>
+          </div>
+          <UiButton>Войти</UiButton>
+          <span className="my5">
+            Забыли пароль ?{" "}
+            <a className="colorBlue" href="">
+              Восстоновить
+            </a>
+          </span>
+          <span className="my5">
+            Не имеете аккаунта ?{" "}
+            <a className="colorBlue" href="">
+              Регистрация
+            </a>
+          </span>
+        </form>
+      </SectionComponent>
+    </div>
   );
 }
 export default LoginPage;
