b22d947621b966e93846644ce50acb4e490dcd2b
Ruben <ruben.abrahamyan00@gmail.com>
2023-05-16 10:27:29 +0900

    m

--- src/components/ui/radioButton/RadioButton.tsx
+++ src/components/ui/radioButton/RadioButton.tsx
@@ -4,25 +4,23 @@ import styles from "../../../pages/profile/ProfilePage.module.css";
 export const RadioButton = ({ children, labelClassName, ...props }) => {
   return (
     <>
-      <div
-        className="width100 r-group "
-        onClick={(e) => {
-          const size = 150,
-            x = e.nativeEvent.offsetX - size / 2,
-            y = e.nativeEvent.offsetY - size / 2,
-            wave = document.createElement("span");
-          wave.className = styles.wave;
+      <input type="radio" {...props} />
+      <div className="tab" onClick={(e) => {
+        const size = 150,
+          x = e.nativeEvent.offsetX - size / 2,
+          y = e.nativeEvent.offsetY - size / 2,
+          wave = document.createElement("span");
+        wave.className = styles.wave;
 
-          wave.style.cssText = `width:${size}px;height:${size}px;top:${y}px;left:${x}px`;
-          e.target.appendChild(wave); 
-          setTimeout(() => wave.remove(), 500)
-        }}
-      >
-        <input type="radio" {...props} />
-        <label htmlFor={props.id} className={labelClassName}>
+        wave.style.cssText = `width:${size}px;height:${size}px;top:${y}px;left:${x}px`;
+        e.target.appendChild(wave);
+        setTimeout(() => wave.remove(), 500)
+      }}>
+        <label
+
+          htmlFor={props.id} className={labelClassName}>
           {children}
-        </label>
-      </div>
+        </label></div>
     </>
   );
 };
--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,13 +1,14 @@
 import { RadioButton } from "../../components/ui/radioButton/RadioButton";
 import SectionComponent from "./../../components/ui/sections/SectionComponent";
 import styles from "./ProfilePage.module.css";
-import { useEffect, useRef, useState } from "react";
-
+import { useEffect, useState } from "react";
+import './ProfilePage.css'
 const tabs = [
   {
     id: 0,
     title: "Colorables",
     name: "tabs",
+    className: 'radio-1',
     content:
       "Nelsons folly lugger marooned hearties chantey lateen sail Yellow Jack gangplank starboard warp. Chantey Buccaneer log warp snow booty sloop square-rigged mutiny pressgang. Yard bring a spring upon her cable Sail ho clipper bounty careen galleon bilge rat reef handsomely.",
   },
@@ -15,6 +16,7 @@ const tabs = [
     id: 1,
     title: "Logos",
     name: "tabs",
+    className: 'radio-2',
     content:
       "Barbary Coast hogshead port Sea Legs cackle fruit dead men tell no tales crimp poop deck transom flogging. Bilge water man-of-war lanyard sloop starboard hardtack no prey, no pay avast tender crack Jennys tea cup. Hogshead cog aft Corsair spanker pinnace bilge water swing the lead Jack Tar Plate Fleet.",
   },
@@ -22,50 +24,36 @@ const tabs = [
     id: 2,
     title: "Blocks",
     name: "tabs",
+    className: 'radio-3',
     content:
       "Gangway boom coffer rigging tackle gabion Shiver me timbers aye draft barque. Scourge of the seven seas pressgang Blimey galleon six pounders yawl grapple jolly boat lugger Jack Ketch. Splice the main brace boatswain snow Chain Shot salmagundi keel crow's nest Shiver me timbers jib ye.",
   },
 ];
 function Tabs({ setActiveTab }) {
- 
-  return (
-    <div
-      className={`br075rem p3 flexMedia450  r-all my20 ${styles.radio_container}`}
-      onChange={(e) => setActiveTab(tabs[e.target.id])}
-    >
-      {tabs.map((el, i) => (
-        <RadioButton
-          key={el.id}
-          labelClassName={`br0375rem ${styles.radioLabel}`}
-          className={`r-input ${styles.radioInput}`}
-          id={el.id}
-          name={el.name}
-          defaultChecked={i == 0 ? "checked" : false}
-        >
-          {el.title}
-        </RadioButton>
-      ))}
-    </div>
+
+  return ( 
+    <div className="flex my20 alignCenter justifyCenter" onChange={(e) => setActiveTab(tabs[e.target.id])}>
+      <div className={`relative flexMedia500 p10 br075rem ${styles.tabs}`}>
+        {tabs.map((tab, i) => ( 
+            <RadioButton
+              key={tab.id}
+              labelClassName={`${styles.tab} ${styles.radioLabel}`}
+              className={`${styles.radioInput} br075rem ${tab['className']}`}
+              id={tab.id}
+              name={tab.name}
+              defaultChecked={i == 0 ? "checked" : false}
+            >
+              {tab.title}
+            </RadioButton> 
+        ))}
+        <span className={`glider br075rem`}></span>
+      </div>
+    </div > 
   );
 }
 
 const ProfilePage = () => {
-  useEffect(() => {
-    setTimeout(() => {
-      let all = document.querySelector(".r-all");  
-        let radios = all.querySelectorAll(".r-input"); 
-        document.documentElement.style.setProperty("--options", radios.length);
-      radios.forEach((input) => {
-        input.setAttribute("data-pos", '1');
-        input.addEventListener("click", (e) => {
-          document.documentElement.style.setProperty(
-            "--options-active",
-            e.target.getAttribute("data-pos")
-          );
-        }); 
-      }); 
-  }, 2000);
-  }, []);
+
   const [activeTab, setActiveTab] = useState(tabs[0]);
   return (
     <>
